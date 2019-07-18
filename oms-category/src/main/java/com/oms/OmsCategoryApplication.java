1d7634e6ed6e6301c8061f1569626f858de15972
Chaithanyachary <chaithanyachary@gmail.com>
2019-07-18 12:29:33 +0530

    hello
    hello

--- oms-category/src/main/java/com/oms/OmsCategoryApplication.java
+++ oms-category/src/main/java/com/oms/OmsCategoryApplication.java
@@ -8,6 +8,7 @@ public class OmsCategoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmsCategoryApplication.class, args);
+		System.out.println("helli");
 	}
 
 }
