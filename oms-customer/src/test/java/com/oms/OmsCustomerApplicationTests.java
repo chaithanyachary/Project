ce5ad0414327fa788dc7d9d434e04e7232d2dff8
Chaithanyachary <chaithanyachary@gmail.com>
2019-07-19 12:04:43 +0530

    modify
    done

--- oms-customer/src/test/java/com/oms/OmsCustomerApplicationTests.java
+++ oms-customer/src/test/java/com/oms/OmsCustomerApplicationTests.java
@@ -2,12 +2,29 @@ package com.oms;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.oms.controller.CustomerController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OmsCustomerApplicationTests {
+	
+	MockMvc mockMvc;
+	
+    @Autowired
+
+    protected WebApplicationContext wac;
+
+    @Autowired
+
+    CustomerController customerController;
+    
+   
 
 	@Test
 	public void contextLoads() {
